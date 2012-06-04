9f224150562cba510285ff473c47d0f7034bb512
TheQwerty <TheQwerty@Gmail.com>
2012-06-04 16:06:04 -0400

    v0.8.5: Better default detection. * Added early return if image is smaller than window. * Cleaned up button action. * Added original / current dimensions to toolbar. * Added guessing for default fit.     Fit dimension if window < dimension < 3*window.     Experiment: Try fitting height first, then if needed fit width.

--- ImageFitter.user.js
+++ ImageFitter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Image Fitter
 // @description    Provides ability to fit framed-images to the window's size.
-// @version        0.8.2
+// @version        0.8.5
 // @author         TheQwerty
 // @namespace      https://github.com/TheQwerty/Misc-UserScripts/raw/master/
 // @homepage       https://github.com/TheQwerty/Misc-UserScripts
@@ -23,17 +23,27 @@
 			var soleChild = window.document.body.children[0];
 			if (soleChild && soleChild.tagName === 'IMG') {
 
+				// Determine if fitting is even needed.
+				var aH = document.body.clientHeight;
+				var aW = document.body.clientWidth;
+				
+				if (soleChild.width <= aW && soleChild.height <= aH)
+					return;
+
 				// Add styles to head (failing back to body)
 				var head, style;
 				head = document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0];
 				if (!head) { return; }
 				var styles = [
+					// Fitting: 95% to account for fitbar.
 					'.width { max-width:95% }',
 					'.height { max-height:95% }',
+					// Toolbar
 					'#fitbar { margin:5px; font-size:12px; }',
 					'#fitbar .btn { margin:2px; padding:2px 5px; background:#866; color:#fff; cursor:pointer; -webkit-border-radius:4px; -moz-border-radius:4px; border-radius:4px; }',
 					'#fitbar .btn:hover, #fitbar .btn:focus { background:#668; }',
-					'#fitbar .btn.active { background:#686; }'
+					'#fitbar .btn.active { background:#686; }',
+					'#fitbar #fitMsgs { margin:0px; float:right; text-align:right; }'
 				];
 				style = document.createElement('style');
 				style.type = 'text/css';
@@ -46,21 +56,18 @@
 					btn.classList.add('btn');
 					btn.textContent = 'Fit to ' + dim;
 					btn.addEventListener('click', function(evt) {
-						// Toggle fitting dimension.
-						var fitting = soleChild.classList.toggle(dim.toLowerCase());
-
-						// Update button state and title.
-						if (fitting) {
+						// Toggle fitting dimension then update button.
+						if (soleChild.classList.toggle(dim.toLowerCase())) {
 							this.title = 'Fitting by ' + dim + '.';
-							if (!this.classList.contains('active')) {
-								this.classList.add('active');
-							}
+							this.classList.add('active');
 						} else {
 							this.title = 'Full ' + dim + '.';
-							if (this.classList.contains('active')) {
-								this.classList.remove('active');
-							}
+							this.classList.remove('active');
 						}
+						
+						// Calculate ratio and display new dimension data.
+						var r = Math.max(soleChild.height, soleChild.width) / Math.min(soleChild.height, soleChild.width);
+						cmsg.textContent = 'Current [View: ' + document.body.clientWidth + 'x' + document.body.clientHeight + '  Image: ' + soleChild.width + 'x' + soleChild.height + ' (' + r.toFixed(2) + ')]';                        
 					});
 
 					// Append to parent.
@@ -68,21 +75,46 @@
 					return btn;
 				};
 
-				// Creates and adds the fitting toolbar.
+				// Creates the fitting toolbar.
 				var fitbar = document.createElement('div');
 				fitbar.id = 'fitbar';
 				var fitHeightBtn = createButton(fitbar, 'Height');
 				var fitWidthBtn = createButton(fitbar, 'Width');
+				
+				// Add dimension messages to toolbar.
+				var msgSpan = document.createElement('span');
+				msgSpan.id = 'fitMsgs';
+				var msg = document.createElement('div');
+				msgSpan.appendChild(msg);
+				var cmsg = document.createElement('div');
+				msgSpan.appendChild(cmsg);
+				fitbar.appendChild(msgSpan);
+				
+				// Insert toolbar.
 				var body = document.getElementsByTagName('body')[0];
 				body.insertBefore(fitbar, body.firstChild);
 
 
-				// TODO: Determine optimal fitting for default.
-				// Maybe determine image aspect ratio and then decide which fitting to apply.
-				// Default state
-				fitWidthBtn.click();
+				// Adjust visible space to account for toolbar's computed height.
+				var fbCS = document.defaultView.getComputedStyle(fitbar, null);
+				var missingHeight = ['margin-top', 'margin-bottom', 'border-top-width', 'border-bottom-width'];
+				for (i in missingHeight) {
+					aH -= parseInt(fbCS.getPropertyValue(missingHeight[i]));
+				}
+				
+				// Calculate ratio and display original dimension data.
+				var r = Math.max(soleChild.height, soleChild.width) / Math.min(soleChild.height, soleChild.width);
+				msg.textContent = 'Original [View: ' + aW + 'x' + aH + '  Image: ' + soleChild.width + 'x' + soleChild.height + ' (' + r.toFixed(2) + ')]';
 
-				// On click cycle: Fit Both > None > Fit Width > Fit Height
+				// Take a stab at guessing the optimal default fitting.
+				var maxFactor = 3;
+				if (soleChild.height >= aH && soleChild.height < maxFactor*aH)
+					fitHeightBtn.click();
+				
+				if (soleChild.width >= aW && soleChild.width < maxFactor*aW)
+					fitWidthBtn.click();
+
+				// Add cycling on image click: Fit Both > None > Fit Width > Fit Height
 				soleChild.addEventListener('click', function(evt) {
 					if (this.classList.contains('width')) {
 						fitHeightBtn.click();
